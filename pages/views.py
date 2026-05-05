757068895ad85daf410e8874486f1e870341f616
Максим Казаринов <kazarinovmaxim@gmail.com>
2026-05-05 17:36:03 +0500

    по пордяку, из внешней папки tests сделал проверку. Поменял ссылки на страницы, вернул старый текст записей но оставил новый, создал новые папки blog и pages для html файлов и удалил папку html, изменил settings, и создал data.py отдельно для списка post. Вроде все

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request, 'html/about.html')  
+    return render(request, 'pages/about.html')  
 
 def rules(request):
-    return render(request, 'html/rules.html')
+    return render(request, 'pages/rules.html')
